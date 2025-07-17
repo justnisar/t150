c6008db9a7b7d874993b4350c5bf795b085b4089
justnisar <justnisar@gmail.com>
2025-07-16 18:21:30 -0700

    ReverseBits

--- src/ReverseBits.java
+++ src/ReverseBits.java
@@ -4,11 +4,17 @@ public class ReverseBits {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
 
+        int result = 0;
         for(int i = 0 ; i < 32 ; i++){
             int currentBit = n & 1;
-            
+
+            n = n >> 1;
+            result = result << 1;
+            result = result | currentBit;
         }
 
-        return 0;
+        return result;
     }
+
+
 }
